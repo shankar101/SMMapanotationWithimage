026342cf8d6d2b16daf2197719a0da94d5aa8e44
Shankar <shankar@shankars-macbook-pro.local>
2016-07-09 08:45:55 +0530

    Initial Commit

---
+++ SMMapwithImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMMapwithImage
+//
+//  Created by Shankar on 09/07/16.
+//  Copyright © 2016 Shankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
